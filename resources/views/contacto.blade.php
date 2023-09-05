c7066df8adef6f43355022f583c4693e2ab8fe58
Isidro-Olivares <isidro.olivares4159@alumnos.udg.mx>
2023-09-04 22:07:16 -0600

    formulario hasta donde dejó el profe en clase 4 septiembre

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -9,7 +9,17 @@
     <h1>Formulario de contacto</h1>
     <form action="" method="POST">
         <label for="correo">CORREO</label><br>
-        
+        <input type="email" name="correo"><br>
+        <textarea name="comentario" id="comentario" cols="30">
+        </textarea>    
+        <br>
+        <label for="tipo">Tipo:</label><br>
+        <select name="tipo">
+            <option value="alumno">Alumno</option>
+            <option value="empleado">Empleado</option>
+        </select>
+        <br>
+        <input type="submit" value="enviar">
     </form>
 </body>
 </html>
